7e19f9fd19b2ae04e890a327189693ce7d849d95
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-21 12:05:08 +0900

    able to capture the user input
    next I'll need to match that with the regex findMatches method

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ function findMatches(wordToMatch, cities) {
   });
 }
 
+function displayMatches() {
+  console.log(this.value);
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
 
 
 // console.log(data);
